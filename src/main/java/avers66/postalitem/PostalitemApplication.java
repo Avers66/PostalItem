ae0fac4c92c754c6913e2603b3d705a105209a87
Alexandr T <avers66@yandex.ru>
2023-08-18 13:46:33 +0500

    structure init

---
+++ src/main/java/avers66/postalitem/PostalitemApplication.java
@@ -0,0 +1,13 @@
+package avers66.postalitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalitemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostalitemApplication.class, args);
+	}
+
+}
